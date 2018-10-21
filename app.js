1159b1849a541e448ee06281c5a76753ca4666da
Johai Kadosh <johaik@gmail.com>
2018-10-21 20:16:37 +0300

    v11

---
+++ app.js
@@ -0,0 +1,58 @@
+require('dotenv').config();
+var express = require("express"),
+    bodyParser = require("body-parser"),
+    request = require("request"),
+    mongoose = require("mongoose"),
+    passport = require("passport"),
+    LocalStrategy = require("passport-local"),
+    Campground = require("./models/campground"),
+    Comment = require("./models/comment"),
+    User = require("./models/user"),
+    flash = require("connect-flash");
+MethodOverride = require("method-override");
+// seedDB = require("./seeds.js");
+
+
+//requring routes
+var commentRoutes = require("./routes/comments"),
+    campgroundRoutes = require("./routes/campgrounds"),
+    indexRoutes = require("./routes/index")
+
+mongoose.connect("mongodb://localhost/yelp_camp", { useNewUrlParser: true });
+var app = express();
+app.set("view engine", "ejs");
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static(__dirname + "/public"));
+app.use(MethodOverride("_method"));
+app.use(flash());
+// seedDB();
+
+//passport config
+app.use(require("express-session")({
+    secret: "C8MGMombN7bYrinsH2rHjA8ofV4C2GIb",
+    resave: false,
+    saveUninitialized: false
+}));
+app.use(passport.initialize());
+app.use(passport.session());
+app.locals.moment = require('moment');
+passport.use(new LocalStrategy(User.authenticate()));
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
+
+
+app.use(function (req, res, next) {
+    res.locals.currentUser = req.user;
+    res.locals.error = req.flash("error");
+    res.locals.success = req.flash("success");
+    next();
+});
+
+app.use("/", indexRoutes);
+app.use("/campgrounds", campgroundRoutes);
+app.use("/campgrounds/:id/comments", commentRoutes);
+
+
+app.listen(3000, 'localhost', function () {
+    console.log("Looks like Connect!");
+});
