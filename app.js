b9cd1bf10f193881b2e667d5281b89259734a7d1
johaik <johai.kadosh@gmail.com>
2018-11-17 10:56:53 +0200

    deploy version

--- app.js
+++ app.js
@@ -10,15 +10,15 @@ var express = require("express"),
     User = require("./models/user"),
     flash = require("connect-flash");
 MethodOverride = require("method-override");
-// seedDB = require("./seeds.js");
+seedDB = require("./seeds.js");
 
 
 //requring routes
 var commentRoutes = require("./routes/comments"),
     campgroundRoutes = require("./routes/campgrounds"),
     indexRoutes = require("./routes/index")
-    mongoose.connect("mongodb://localhost/yelp_camp", { useNewUrlParser: true });
-// mongoose.connect("mongodb://<dbuser>:<dbpassword>@ds219040.mlab.com:19040/yelpcmapdb", { useNewUrlParser: true });
+    // mongoose.connect("mongodb://localhost/yelp_camp", { useNewUrlParser: true });
+mongoose.connect("mongodb://johaik:"+process.env.DB_PASSWORD+"@ds219040.mlab.com:19040/yelpcmapdb", { useNewUrlParser: true });
 
 var app = express();
 app.set("view engine", "ejs");
@@ -26,7 +26,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static(__dirname + "/public"));
 app.use(MethodOverride("_method"));
 app.use(flash());
-// seedDB();
+seedDB();
 
 //passport config
 app.use(require("express-session")({
@@ -54,6 +54,6 @@ app.use("/campgrounds", campgroundRoutes);
 app.use("/campgrounds/:id/comments", commentRoutes);
 
 
-app.listen(process.env.PORT, process.env.IP, function () {
+app.listen(3000, process.env.IP, function () {
     console.log("Looks like Connect!");
 });
